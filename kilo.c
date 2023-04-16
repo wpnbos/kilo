04ed8bf0d52da4373cede2673ca54570aed5ebcd
William <william@unetiq.com>
2023-04-16 12:57:39 +0200

    Add vertical and horizontal scrolling

--- kilo.c
+++ kilo.c
@@ -44,6 +44,8 @@ enum editorKey
 struct editorConfig
 {
     int cx, cy;
+    int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -276,13 +278,35 @@ void abFree(struct abuf *ab)
 }
 /*** output ***/
 
+void editorScroll()
+{
+    if (E.cy < E.rowoff)
+    {
+        E.rowoff = E.cy;
+    }
+    if (E.cy >= E.rowoff + E.screenrows)
+    {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+    if (E.cx >= E.coloff + E.screencols)
+    {
+        E.coloff = E.cx - E.screencols + 1;
+    }
+    if (E.cx < E.coloff)
+    {
+        E.coloff = E.cx;
+    }
+}
+
 void editorDrawRows(struct abuf *ab)
 {
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
-        if (y >= E.numrows)
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows)
         {
+            // Render welcome message
             if (E.numrows == 0 && y == E.screenrows / 3)
             {
                 char welcome[80];
@@ -300,17 +324,21 @@ void editorDrawRows(struct abuf *ab)
                     abAppend(ab, " ", 1);
                 abAppend(ab, welcome, welcomelen);
             }
+            // Render tildes
             else
             {
                 abAppend(ab, "~", 1);
             }
         }
+        // Render file lines
         else
         {
-            int len = E.row[y].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0)
+                len = 0;
             if (len > E.screencols)
                 len = E.screencols;
-            abAppend(ab, E.row[y].chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
         abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1)
@@ -322,6 +350,8 @@ void editorDrawRows(struct abuf *ab)
 
 void editorRefreshScreen()
 {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
@@ -330,7 +360,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1 - E.rowoff, E.cx + 1 - E.coloff);
     abAppend(&ab, buf, strlen(buf));
     abAppend(&ab, "\x1b[?25h", 6);
 
@@ -353,12 +383,11 @@ void editorMoveCursor(int key)
             E.cx--;
         break;
     case ARROW_DOWN:
-        if (E.cy < E.screenrows - 1)
+        if (E.cy < E.numrows)
             E.cy++;
         break;
     case ARROW_RIGHT:
-        if (E.cx < E.screencols - 1)
-            E.cx++;
+        E.cx++;
         break;
     }
 }
@@ -404,6 +433,8 @@ void initEditor()
 {
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
